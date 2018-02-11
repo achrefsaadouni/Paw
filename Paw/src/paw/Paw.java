cb15ae92c1d6ea0934da8cdd71f09a938c5808f5
AYOUB <ayoub.elkadhi@esprit.tn>
2018-02-11 19:37:28 +0100

    undecorated skin

--- Paw/src/paw/Paw.java
+++ Paw/src/paw/Paw.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -22,8 +23,10 @@ public class Paw extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
+      
+        stage.initStyle(StageStyle.UNDECORATED);
+                stage.setScene(scene);
+        //stage.setFullScreen(true);
         stage.show();
     }
 
