2298d90f10a6b5cc6c41efc32ec3e9395a4fba2b
AYOUB <ayoub.elkadhi@esprit.tn>
2018-02-13 02:17:05 +0100

    Ajout Hamburger et menu

--- Paw/src/paw/mainUI/FXMLCnxController.java
+++ Paw/src/paw/mainUI/FXMLCnxController.java
@@ -6,10 +6,14 @@
 package paw.mainUI;
 
 import Entity.Utilisateur;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,6 +21,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import static paw.Paw.session;
 
 /**
@@ -26,46 +31,72 @@ import static paw.Paw.session;
 public class FXMLCnxController implements Initializable {
 
     @FXML
-    private ImageView avatar;
-    @FXML
-    private ImageView avatar1;
-    @FXML
     private AnchorPane window;
-     
+    @FXML
+    private JFXDrawer drawer;     
     @FXML
     private Label username;
     @FXML
     private Label email;
+    @FXML
+    private ImageView settings;
+    @FXML
+    private ImageView profile;
+    @FXML
+    private ImageView notification;
+    @FXML
+    private ImageView chat;
+    @FXML
+    private JFXHamburger hamburger;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        username.setText(session.getUsername());
-        email.setText(session.getEmail());
+        try {
+            username.setText(session.getUsername());
+            email.setText(session.getEmail());
+            HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+            transition.setRate(-1);
+            AnchorPane menu = FXMLLoader.load(getClass().getResource("/paw/mainUI/FXMLMenu.fxml"));         
+            drawer.setSidePane(menu);
+            hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+                transition.setRate(transition.getRate()* -1);
+                transition.play();
+                drawer.toggle();
+            });
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLCnxController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
-
-
-    @FXML
-    private void goToGris(ActionEvent event) throws IOException {
-        //window.getChildren().setAll(FXMLLoader.load(getClass().getResource("FXMLutilisateur.fxml")));
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("/paw/mainUI/FXMLGris.fxml"));
-        window.getChildren().setAll(pane);
+    public void initialisation(Utilisateur x) {
+        session=x;  
     }
 
+//    private void goToGris(ActionEvent event) throws IOException {
+//        AnchorPane pane = FXMLLoader.load(getClass().getResource("/paw/mainUI/FXMLGris.fxml"));
+//        window.getChildren().setAll(pane);
+//    }
+//
+//    private void goToRed(ActionEvent event) throws IOException {
+//        AnchorPane pane = FXMLLoader.load(getClass().getResource("/paw/mainUI/FXMLRed.fxml"));
+//        window.getChildren().setAll(pane);
+//    }
+
     @FXML
-    private void goToRed(ActionEvent event) throws IOException {
+    private void goToProfile(MouseEvent event) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/paw/mainUI/FXMLRed.fxml"));
         window.getChildren().setAll(pane);
-    }
-
-    @FXML
-    private void goToProfile(MouseEvent event) {
     }
 
     @FXML
     private void goToNotification(MouseEvent event) {
     }
 
-    public void initialisation(Utilisateur x) {
-        session=x;  
+    @FXML
+    private void goToSettings(MouseEvent event) {
+    }
+
+    @FXML
+    private void goToChat(MouseEvent event) {
     }
 
     
