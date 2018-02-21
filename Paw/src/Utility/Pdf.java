b3605b0cc6c73ed11acd6f048e8a0164eb68cab1
achref <achref.saadouni@esprit.tn>
2018-02-21 23:40:42 +0100

    aaaa

---
+++ Paw/src/Utility/Pdf.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utility;
+
+import Entity.Achat;
+import Entity.LigneAchat;
+import Entity.Utilisateur;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author achref
+ */
+public class Pdf {
+
+    public static final String dest = "pdf/";
+    public static final String IMAGE = "pdf/background_image.jpg";
+    public static final Font BLUE_BOLD = new Font(FontFamily.HELVETICA, 20, Font.BOLD, BaseColor.BLUE);
+    public static final Chunk titre = new Chunk("Bon de Livraison", BLUE_BOLD);
+    public static final Chunk aa = new Chunk("Achat", BLUE_BOLD);
+
+    public static void getPdf(Achat a, Utilisateur u) {
+
+        try {
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest + a.getId_achat() + ".pdf"));
+            document.open();
+            Paragraph p1 = new Paragraph(titre);
+            p1.setAlignment(Element.ALIGN_CENTER);
+            PdfContentByte canvas = writer.getDirectContentUnder();
+            Image ima;
+            try {
+                ima = Image.getInstance("pdf/logo.png");
+                ima.scalePercent(30);
+                document.add(ima);
+            } catch (BadElementException ex) {
+                Logger.getLogger(Pdf.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Pdf.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            document.add(p1);
+            try {
+                Image image = Image.getInstance(IMAGE);
+                image.scaleAbsolute(PageSize.A4);
+                image.setAbsolutePosition(0, 0);
+                canvas.addImage(image);
+            } catch (BadElementException ex) {
+                Logger.getLogger(Pdf.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Pdf.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Paragraph p2 = new Paragraph("Achat N " + a.getId_achat());
+            Paragraph p3 = new Paragraph("Pour Mr/Madame : " + u.getNom() + " " + u.getPrenom());
+            Paragraph p4 = new Paragraph("Passer Le : " + a.getDate_achat().toString());
+            p4.setAlignment(Element.ALIGN_RIGHT);
+            Paragraph p5 = new Paragraph("Etat de L'achat : " + a.getEtat());
+            Paragraph p6 = new Paragraph("Adresse du Client : " + u.getAddresse());
+            Paragraph p7 = new Paragraph("Numero Telephone : " + u.getNumero());
+            PdfPTable table = new PdfPTable(3);
+            PdfPCell cell = new PdfPCell(new Paragraph(aa));
+            cell.setColspan(4);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+            table.addCell("Libelle");
+            table.addCell("Quantite");
+            table.addCell("Prix Unitaire");
+            for (LigneAchat ligneAchat : a.getList()) {
+                table.addCell(ligneAchat.getProduit().getLibelle());
+                table.addCell(String.valueOf(ligneAchat.getNbr_produit()));
+                table.addCell(String.valueOf(ligneAchat.getProduit().getPrix()));
+
+            }
+            Paragraph p8 = new Paragraph(" ");
+            Paragraph p9 = new Paragraph(" ");
+
+            PdfPTable table2 = new PdfPTable(2);
+            table2.addCell("Livrer le : ");
+            table2.addCell("Signature Du Client");
+            table2.addCell("           ");
+            table2.addCell("           ");
+            document.add(p2);
+            document.add(new Paragraph("  "));
+            document.add(p3);
+            document.add(new Paragraph("  "));
+            document.add(p4);
+            document.add(new Paragraph("  "));
+            document.add(p5);
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("Prix Total de : " + a.getPrix()));
+            document.add(new Paragraph("  "));
+            document.add(p6);
+            document.add(new Paragraph("  "));
+            document.add(p7);
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(table);
+            document.add(p8);
+            document.add(p9);
+            document.add(new Paragraph("  "));
+            document.add(new Paragraph("  "));
+            document.add(table2);
+            document.close();
+        } catch (FileNotFoundException | DocumentException ex) {
+            Logger.getLogger(Pdf.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
--- Paw/src/paw/boutique/admin/achat/FXMLAchatController.java
+++ Paw/src/paw/boutique/admin/achat/FXMLAchatController.java
@@ -11,6 +11,7 @@ import Entity.Utilisateur;
 import Service.AchatService;
 import Service.LigneAchatService;
 import Service.UtilisateurServices;
+import Utility.Pdf;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXTextField;
@@ -18,10 +19,13 @@ import com.jfoenix.controls.JFXTreeTableView;
 import com.jfoenix.controls.RecursiveTreeItem;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import java.util.List;
+import java.util.function.Predicate;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -37,7 +41,7 @@ import javafx.scene.layout.StackPane;
 import paw.MyNotifications;
 
 public class FXMLAchatController {
-    
+
     List<Achat> liste_achat;
     @FXML
     private JFXTextField recherche_achat;
@@ -71,7 +75,7 @@ public class FXMLAchatController {
 
     @FXML
     private TreeTableColumn<LigneAchat, Number> nombre_achat;
-        @FXML
+    @FXML
     private TreeTableColumn<Achat, String> adresse;
 
     @FXML
@@ -96,31 +100,33 @@ public class FXMLAchatController {
     private JFXButton annuler;
     @FXML
     private JFXButton ok_supprimer;
+
     public void initialize() {
-        
+
         info.setVisible(true);
         ligneachat.setVisible(false);
         rechercher_ligneachat.setVisible(false);
         initAchat();
-    }    
-    
-    public void initAchat()
-    {
-         liste_achat =achatservice.All();
-         AchatService achatservice = AchatService.getAchatService();
-         UtilisateurServices p = new UtilisateurServices();
-         
+        supprimer.setVisible(false);
+        pdf.setVisible(false);
+        livrer.setVisible(false);
+    }
+
+    public void initAchat() {
+        liste_achat = achatservice.All();
+        AchatService achatservice = AchatService.getAchatService();
+        UtilisateurServices p = new UtilisateurServices();
+
         nom_prenom.setCellValueFactory(param -> {
             SimpleStringProperty property = new SimpleStringProperty();
             Achat achat = (Achat) param.getValue().getValue();
             Utilisateur u = null;
             u = p.rechercher(achat.getId_client());
-            property.set(u.getNom()+" "+u.getPrenom());
+            property.set(u.getNom() + " " + u.getPrenom());
             return property;
         });
-        
-        
-            date.setCellValueFactory(param -> {
+
+        date.setCellValueFactory(param -> {
             SimpleStringProperty property = new SimpleStringProperty();
             Achat achat = (Achat) param.getValue().getValue();
             property.setValue(achat.getDate_achat().toString());
@@ -132,64 +138,83 @@ public class FXMLAchatController {
             property.setValue(achat.getEtat());
             return property;
         });
-        
-          prixTotal.setCellValueFactory(param -> {
+
+        prixTotal.setCellValueFactory(param -> {
             SimpleDoubleProperty property = new SimpleDoubleProperty();
             Achat achat = (Achat) param.getValue().getValue();
             property.set(achat.getPrix());
             return property;
         });
-          
-          
-              adresse.setCellValueFactory(param -> {
+
+        adresse.setCellValueFactory(param -> {
             SimpleStringProperty property = new SimpleStringProperty();
             Achat achat = (Achat) param.getValue().getValue();
-             Utilisateur u = null;
+            Utilisateur u = null;
             u = p.rechercher(achat.getId_client());
             property.set(u.getAddresse());
             return property;
         });
-                  numero.setCellValueFactory(param -> {
+        numero.setCellValueFactory(param -> {
             SimpleStringProperty property = new SimpleStringProperty();
             Achat achat = (Achat) param.getValue().getValue();
-             Utilisateur u = null;
+            Utilisateur u = null;
             u = p.rechercher(achat.getId_client());
             property.set(String.valueOf(u.getNumero()));
             return property;
         });
-          
+
         achat.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-            if(newSelection==null)
-            {
+            if (newSelection == null) {
                 consulter(oldSelection.getValue().getList());
-            }
-            else
+            } else {
                 consulter(newSelection.getValue().getList());
-                });
- 
-       refresh();
+            }
+            supprimer.setVisible(true);
+            pdf.setVisible(true);
+            livrer.setVisible(true);
+        });
         
+        
+        
+            recherche_achat.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                achat.setPredicate(new Predicate<TreeItem<Achat>>() {
+                    @Override
+                    public boolean test(TreeItem<Achat> t) {
+                        UtilisateurServices p = new UtilisateurServices();
+                        Utilisateur u = p.rechercher(t.getValue().getId_client());
+                        boolean flag = (u.getNom().concat(u.getPrenom())).toUpperCase().contains(newValue.toUpperCase()) || String.valueOf(t.getValue().getPrix()).equals(newValue) || String.valueOf(t.getValue().getDate_achat()).equals(newValue) || t.getValue().getEtat().contains(newValue)||u.getAddresse().toUpperCase().contains(newValue.toUpperCase())||String.valueOf(u.getNumero()).equals(newValue);
+                        return flag;
+                    }
+                });
+            }
+        });
+        
+        
+        
+        
+        refresh();
+
     }
-    public void refresh()
-    {
-        liste_achat =achatservice.All();
+
+    public void refresh() {
+        liste_achat = achatservice.All();
         ObservableList<Achat> achats = FXCollections.observableArrayList(liste_achat);
         TreeItem<Achat> root = new RecursiveTreeItem<>(achats, RecursiveTreeObject::getChildren);
         achat.setRoot(root);
         achat.setShowRoot(false);
     }
-    
-    
-      public void consulter(List<LigneAchat> liste)
-    {  
-        
-            LigneAchatService achatservice = LigneAchatService.getLigneService();
-             image.setCellValueFactory(param -> {
+
+    public void consulter(List<LigneAchat> liste) {
+
+        LigneAchatService achatservice = LigneAchatService.getLigneService();
+        image.setCellValueFactory(param -> {
             SimpleObjectProperty property = new SimpleObjectProperty();
             LigneAchat ligne = (LigneAchat) param.getValue().getValue();
             ImageView im = new ImageView();
             try {
-              Image  img = new Image("file:///" + ligne.getProduit().getImages().get(0).getPath().toString());
+                Image img = new Image("file:///" + ligne.getProduit().getImages().get(0).getPath().toString());
                 im.setFitHeight(100);
                 im.setFitWidth(100);
                 im.setImage(img);
@@ -206,73 +231,87 @@ public class FXMLAchatController {
             property.set(achat.getProduit().getLibelle());
             return property;
         });
-        
-        
-            nombre_achat.setCellValueFactory(param -> {
+
+        nombre_achat.setCellValueFactory(param -> {
             SimpleIntegerProperty property = new SimpleIntegerProperty();
             LigneAchat achat = (LigneAchat) param.getValue().getValue();
             property.setValue(achat.getNbr_produit());
             return property;
         });
-        
-        
-          prixunite.setCellValueFactory(param -> {
+
+        prixunite.setCellValueFactory(param -> {
             SimpleDoubleProperty property = new SimpleDoubleProperty();
             LigneAchat achat = (LigneAchat) param.getValue().getValue();
-            property.set(achat.getProduit().getPrix()*achat.getNbr_produit());
+            property.set(achat.getProduit().getPrix() * achat.getNbr_produit());
             return property;
         });
-          ligneachat.setVisible(true);
-          rechercher_ligneachat.setVisible(true);
-          info.setVisible(false);
+        ligneachat.setVisible(true);
+        rechercher_ligneachat.setVisible(true);
+        
+             rechercher_ligneachat.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                ligneachat.setPredicate(new Predicate<TreeItem<LigneAchat>>() {
+                    @Override
+                    public boolean test(TreeItem<LigneAchat> t) {
+                      
+                        boolean flag = (t.getValue().getProduit().getLibelle()).toUpperCase().contains(newValue.toUpperCase()) || String.valueOf(t.getValue().getProduit().getPrix()).equals(newValue) || String.valueOf(t.getValue().getNbr_produit()).equals(newValue);
+                        return flag;
+                    }
+                });
+            }
+        });
+        
+        info.setVisible(false);
         ObservableList<LigneAchat> achats = FXCollections.observableArrayList(liste);
         TreeItem<LigneAchat> root = new RecursiveTreeItem<>(achats, RecursiveTreeObject::getChildren);
         ligneachat.setRoot(root);
         ligneachat.setShowRoot(false);
         info.setVisible(false);
-        
+
     }
 
     @FXML
     private void supprimer(ActionEvent event) {
- 
+
         stackpane.setVisible(true);
 
-        
     }
 
     @FXML
     private void pdf(ActionEvent event) {
-        
+        Achat a = achat.getSelectionModel().selectedItemProperty().getValue().getValue();
+        UtilisateurServices p = new UtilisateurServices();
+        Utilisateur u = p.rechercher(a.getId_client());
+        Pdf.getPdf(a, u);
     }
 
     @FXML
     private void livrer(ActionEvent event) {
-       Achat a = achat.getSelectionModel().selectedItemProperty().getValue().getValue();
+        Achat a = achat.getSelectionModel().selectedItemProperty().getValue().getValue();
         System.out.println(a.getEtat());
-        if("livrer".equals(a.getEtat()))
-        {
-             MyNotifications.infoNotification("Modification", "Cet Achat a ete deja livre");
-             return;
+        if ("livrer".equals(a.getEtat())) {
+            MyNotifications.infoNotification("Modification", "Cet Achat a ete deja livre");
+            return;
         }
-        
-                achatservice.livrer(a.getId_achat());
-                MyNotifications.infoNotification("Modification", "Cet Achat A ete modifier a l'etat livrer");
-               refresh();
+
+        achatservice.livrer(a.getId_achat());
+        MyNotifications.infoNotification("Modification", "Cet Achat A ete modifier a l'etat livrer");
+        refresh();
     }
 
     @FXML
     private void annuler(ActionEvent event) {
-         stackpane.setVisible(false);
+        stackpane.setVisible(false);
     }
 
     @FXML
     private void confirmersupprimer(ActionEvent event) {
-                Achat a = achat.getSelectionModel().selectedItemProperty().getValue().getValue();
-                achatservice.deleteAchat(a);
-                MyNotifications.infoNotification("Suppression", "Cet Achat A ete supprimer ");
-                refresh();
-                stackpane.setVisible(false);
+        Achat a = achat.getSelectionModel().selectedItemProperty().getValue().getValue();
+        achatservice.deleteAchat(a);
+        MyNotifications.infoNotification("Suppression", "Cet Achat A ete supprimer ");
+        refresh();
+        stackpane.setVisible(false);
     }
-    
+
 }
