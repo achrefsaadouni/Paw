66e156c537d7efc61f45dca5fa41804e6ae6121a
Mehdi Gaaloul <mehdi.gaaloul@esprit.tn>
2018-02-23 18:01:58 +0100

    api gps veterinaire

--- Paw/src/paw/veterinaires/admin/FXMLAjoutVeterinaireController.java
+++ Paw/src/paw/veterinaires/admin/FXMLAjoutVeterinaireController.java
@@ -5,24 +5,29 @@
  */
 package paw.veterinaires.admin;
 
-import Entity.Veterinaire;
-import com.lynden.gmapsfx.ClusteredGoogleMapView;
+import com.jfoenix.controls.JFXTextField;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.GoogleMap;
 import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.javascript.object.MapOptions;
 import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
-
-
-
-
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
+import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
+import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
+import com.lynden.gmapsfx.service.geocoding.GeocodingService;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import netscape.javascript.JSObject;
 
 /**
  * FXML Controller class
@@ -31,10 +36,16 @@ import javafx.fxml.Initializable;
  */
 public class FXMLAjoutVeterinaireController implements Initializable, MapComponentInitializedListener {
 
-    private GoogleMap map;
-    private GoogleMapView mapView;
     @FXML
-    private ClusteredGoogleMapView carte;
+    private GoogleMapView mapView;
+
+    private GoogleMap map;
+    @FXML
+    private TextField addressTextField;
+
+    private GeocodingService geocodingService;
+
+    private StringProperty address = new SimpleStringProperty();
 
     /**
      * Initializes the controller class.
@@ -42,13 +53,64 @@ public class FXMLAjoutVeterinaireController implements Initializable, MapCompone
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         mapView.addMapInializedListener(this);
+        address.bind(addressTextField.textProperty());
     }
-    
-   
-    
+
     @Override
     public void mapInitialized() {
 
+        geocodingService = new GeocodingService();
+        MapOptions mapOptions = new MapOptions();
 
+        mapOptions.center(new LatLong(47.6097, -122.3331))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(12);
+
+        map = mapView.createMap(mapOptions);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        map.addUIEventHandler(UIEventType.click, (JSObject obj) -> {
+            LatLong ll = new LatLong((JSObject) obj.getMember("latLng"));
+
+            markerOptions.position(new LatLong(ll.getLatitude(), ll.getLongitude()))
+                    .visible(Boolean.TRUE)
+                    .title("My Marker");
+
+            Marker marker = new Marker(markerOptions);
+
+            map.addMarker(marker);
+            System.out.println("LatLong: lat: " + ll.getLatitude() + " lng: " + ll.getLongitude());
+        });
+
+    }
+
+    @FXML
+    private void addressTextFieldAction(ActionEvent event) {
+        geocodingService.geocode(address.get(), (GeocodingResult[] results, GeocoderStatus status) -> {
+
+            LatLong latLong = null;
+
+            if (status == GeocoderStatus.ZERO_RESULTS) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No matching address found");
+                alert.show();
+                return;
+            } else if (results.length > 1) {
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Multiple results found, showing the first one.");
+                alert.show();
+                latLong = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
+            } else {
+                latLong = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
+            }
+
+            map.setCenter(latLong);
+
+        });
     }
 }
