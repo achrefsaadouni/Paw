594d421dbfbf2f7b84fedd97a34bc54fd27f192c
Mehdi Gaaloul <mehdi.gaaloul@esprit.tn>
2018-02-25 00:36:29 +0100

    position vets fixed

--- Paw/src/paw/veterinaires/FXMLVeterinairesController.java
+++ Paw/src/paw/veterinaires/FXMLVeterinairesController.java
@@ -4,6 +4,13 @@ import Entity.Vets;
 import Service.VeterinaireServices;
 import com.jfoenix.controls.JFXButton;
 import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -21,7 +28,7 @@ import javafx.scene.layout.StackPane;
 import org.controlsfx.control.Rating;
 import static paw.Paw.session;
 
-public class FXMLVeterinairesController implements Initializable {
+public class FXMLVeterinairesController implements Initializable, MapComponentInitializedListener {
 
     @FXML
     private Label nom1;
@@ -122,14 +129,26 @@ public class FXMLVeterinairesController implements Initializable {
     private GoogleMapView mapView;
     @FXML
     private StackPane posVet;
+    @FXML
+    private JFXButton position1;
+    @FXML
+    private JFXButton position2;
+    @FXML
+    private JFXButton position3;
+    @FXML
+    private JFXButton position4;
 
+    private GoogleMap map;
+    
+    LatLong x;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         VeterinaireServices service = new VeterinaireServices();
 
         list = service.getList();
-        System.out.println(list);
+        
         if (list.isEmpty()) {
             box1.setVisible(false);
             box2.setVisible(false);
@@ -144,6 +163,7 @@ public class FXMLVeterinairesController implements Initializable {
             vide.setVisible(false);
 
         }
+        mapView.addMapInializedListener(this);
 
     }
 
@@ -172,7 +192,7 @@ public class FXMLVeterinairesController implements Initializable {
         }
         return list.subList(0, 3);
     }
-
+    
     private void initVeterinairePage(int index) {
         VeterinaireServices service1 = new VeterinaireServices();
 
@@ -180,6 +200,7 @@ public class FXMLVeterinairesController implements Initializable {
         DecimalFormat df = new DecimalFormat("#.#");
         paginator.setCurrentPageIndex(index);
         List<Vets> QuatreVeterinaires = getVeterinairesPage(index);
+        
         if (QuatreVeterinaires.size() >= 1) {
             box1.setVisible(true);
 
@@ -194,36 +215,50 @@ public class FXMLVeterinairesController implements Initializable {
             } else {
                 note1.setText(String.valueOf((QuatreVeterinaires.get(0).getRate()).intValue()) + "/5");
             }
-
+            
             eval1.setOnAction((ActionEvent)
                     -> {
                 docteur.setText("Evaluer Dr." + QuatreVeterinaires.get(0).getNom() + " " + QuatreVeterinaires.get(0).getPrenom());
                 evaluer.setVisible(true);
-                
-                
+
                 evaluation1.setVisible(false);
                 evaluation2.setVisible(false);
                 evaluation3.setVisible(false);
                 evaluation.setVisible(true);
-                
-                
+
                 VeterinaireServices service = new VeterinaireServices();
-                evaluation.setRating(service.getRating(session.getId(),QuatreVeterinaires.get(0).getId()));
-                        
+                evaluation.setRating(service.getRating(session.getId(), QuatreVeterinaires.get(0).getId()));
+
                 evaluation.ratingProperty().addListener(new ChangeListener<Number>() {
                     @Override
                     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                         VeterinaireServices service = new VeterinaireServices();
-                        if(service.getRating(session.getId(), QuatreVeterinaires.get(0).getId())!=0){
-                            service.updateRating(newValue.intValue(), session.getId(),QuatreVeterinaires.get(0).getId());
-                        }
-                        else{
+                        if (service.getRating(session.getId(), QuatreVeterinaires.get(0).getId()) != 0) {
+                            service.updateRating(newValue.intValue(), session.getId(), QuatreVeterinaires.get(0).getId());
+                        } else {
                             service.setRating(QuatreVeterinaires.get(0).getId(), session.getId(), newValue.intValue());
                         }
                     }
 
                 });
             });
+            
+            position1.setOnAction((ActionEvent)
+                    -> {
+                
+                posVet.setVisible(true);
+                map.clearMarkers();
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(new LatLong(QuatreVeterinaires.get(0).getLatitude(), QuatreVeterinaires.get(0).getLongitude()))
+                        .visible(Boolean.TRUE)
+                        .title("My Marker");
+
+                Marker marker = new Marker(markerOptions);
+
+                map.addMarker(marker);
+
+            });
+
         } else {
             box1.setVisible(false);
         }
@@ -250,27 +285,42 @@ public class FXMLVeterinairesController implements Initializable {
                 evaluer.setVisible(true);
 
                 VeterinaireServices service = new VeterinaireServices();
-                evaluation1.setRating(service.getRating(session.getId(),QuatreVeterinaires.get(1).getId()));
-                
+                evaluation1.setRating(service.getRating(session.getId(), QuatreVeterinaires.get(1).getId()));
+
                 evaluation.setVisible(false);
                 evaluation2.setVisible(false);
                 evaluation3.setVisible(false);
                 evaluation1.setVisible(true);
-                
+
                 evaluation1.ratingProperty().addListener(new ChangeListener<Number>() {
                     @Override
                     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                         VeterinaireServices service = new VeterinaireServices();
-                        if(service.getRating(session.getId(), QuatreVeterinaires.get(1).getId())!=0){
-                            service.updateRating(newValue.intValue(), session.getId(),QuatreVeterinaires.get(1).getId());
-                        }
-                        else{
+                        if (service.getRating(session.getId(), QuatreVeterinaires.get(1).getId()) != 0) {
+                            service.updateRating(newValue.intValue(), session.getId(), QuatreVeterinaires.get(1).getId());
+                        } else {
                             service.setRating(QuatreVeterinaires.get(1).getId(), session.getId(), newValue.intValue());
                         }
                     }
 
                 });
 
+            });
+
+            position2.setOnAction((ActionEvent)
+                    -> {
+                posVet.setVisible(true);
+                map.clearMarkers();
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                markerOptions.position(new LatLong(QuatreVeterinaires.get(1).getLatitude(), QuatreVeterinaires.get(1).getLongitude()))
+                        .visible(Boolean.TRUE)
+                        .title("My Marker");
+
+                Marker marker = new Marker(markerOptions);
+
+                map.addMarker(marker);
+                //System.out.println(QuatreVeterinaires.get(1).getLatitude()+"    "+QuatreVeterinaires.get(1).getLongitude());
             });
 
         } else {
@@ -297,11 +347,10 @@ public class FXMLVeterinairesController implements Initializable {
                     -> {
                 docteur.setText("Evaluer Dr." + QuatreVeterinaires.get(2).getNom() + " " + QuatreVeterinaires.get(2).getPrenom());
                 evaluer.setVisible(true);
-                
+
                 VeterinaireServices service = new VeterinaireServices();
-                evaluation2.setRating(service.getRating(session.getId(),QuatreVeterinaires.get(2).getId()));
-                
-                
+                evaluation2.setRating(service.getRating(session.getId(), QuatreVeterinaires.get(2).getId()));
+
                 evaluation1.setVisible(false);
                 evaluation.setVisible(false);
                 evaluation3.setVisible(false);
@@ -310,10 +359,9 @@ public class FXMLVeterinairesController implements Initializable {
                     @Override
                     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                         VeterinaireServices service = new VeterinaireServices();
-                        if(service.getRating(session.getId(), QuatreVeterinaires.get(2).getId())!=0){
-                            service.updateRating(newValue.intValue(), session.getId(),QuatreVeterinaires.get(2).getId());
-                        }
-                        else{
+                        if (service.getRating(session.getId(), QuatreVeterinaires.get(2).getId()) != 0) {
+                            service.updateRating(newValue.intValue(), session.getId(), QuatreVeterinaires.get(2).getId());
+                        } else {
                             service.setRating(QuatreVeterinaires.get(2).getId(), session.getId(), newValue.intValue());
                         }
                     }
@@ -321,6 +369,25 @@ public class FXMLVeterinairesController implements Initializable {
                 });
 
             });
+            
+            
+            position3.setOnAction((ActionEvent)
+                    -> {
+                posVet.setVisible(true);
+                map.clearMarkers();
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                markerOptions.position(new LatLong(QuatreVeterinaires.get(2).getLatitude(), QuatreVeterinaires.get(2).getLongitude()))
+                        .visible(Boolean.TRUE)
+                        .title("My Marker");
+
+                Marker marker = new Marker(markerOptions);
+
+                map.addMarker(marker);
+                //System.out.println(QuatreVeterinaires.get(1).getLatitude()+"    "+QuatreVeterinaires.get(1).getLongitude());
+            });
+            
+            
 
         } else {
             box3.setVisible(false);
@@ -328,9 +395,7 @@ public class FXMLVeterinairesController implements Initializable {
 
         ///////////////////////////////////////////////////////////
         if (QuatreVeterinaires.size() >= 4) {
-            
-            
-            
+
             box4.setVisible(true);
 
             nom4.setText(QuatreVeterinaires.get(3).getNom() + " " + QuatreVeterinaires.get(3).getPrenom());
@@ -345,28 +410,25 @@ public class FXMLVeterinairesController implements Initializable {
                 note4.setText(String.valueOf((QuatreVeterinaires.get(3).getRate()).intValue()) + "/5");
             }
 
-            
             eval4.setOnAction((ActionEvent)
                     -> {
                 docteur.setText("Evaluer Dr. " + QuatreVeterinaires.get(3).getNom() + " " + QuatreVeterinaires.get(3).getPrenom());
                 evaluer.setVisible(true);
                 VeterinaireServices service = new VeterinaireServices();
-                evaluation3.setRating(service.getRating(session.getId(),QuatreVeterinaires.get(3).getId()));
-                
-                
+                evaluation3.setRating(service.getRating(session.getId(), QuatreVeterinaires.get(3).getId()));
+
                 evaluation1.setVisible(false);
                 evaluation2.setVisible(false);
                 evaluation.setVisible(false);
                 evaluation3.setVisible(true);
-                
+
                 evaluation3.ratingProperty().addListener(new ChangeListener<Number>() {
                     @Override
                     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                         VeterinaireServices service = new VeterinaireServices();
-                        if(service.getRating(session.getId(), QuatreVeterinaires.get(3).getId())!=0){
-                            service.updateRating(newValue.intValue(), session.getId(),QuatreVeterinaires.get(3).getId());
-                        }
-                        else{
+                        if (service.getRating(session.getId(), QuatreVeterinaires.get(3).getId()) != 0) {
+                            service.updateRating(newValue.intValue(), session.getId(), QuatreVeterinaires.get(3).getId());
+                        } else {
                             service.setRating(QuatreVeterinaires.get(3).getId(), session.getId(), newValue.intValue());
                         }
                     }
@@ -374,13 +436,29 @@ public class FXMLVeterinairesController implements Initializable {
                 });
 
             });
+            
+            position4.setOnAction((ActionEvent)
+                    -> {
+                posVet.setVisible(true);
+
+                map.clearMarkers();
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                markerOptions.position(new LatLong(QuatreVeterinaires.get(3).getLatitude(), QuatreVeterinaires.get(3).getLongitude()))
+                        .visible(Boolean.TRUE)
+                        .title("My Marker");
+
+                Marker marker = new Marker(markerOptions);
+
+                map.addMarker(marker);
+              
+            });
 
         } else {
             box4.setVisible(false);
         }
 
     }
-
 
     @FXML
     private void actionRate(ActionEvent event) {
@@ -392,11 +470,31 @@ public class FXMLVeterinairesController implements Initializable {
     private void fermer(ActionEvent event) {
         initVeterinairePage(paginator.getCurrentPageIndex());
         evaluer.setVisible(false);
+        posVet.setVisible(false);
 
     }
 
     @FXML
     private void actionPosition(ActionEvent event) {
+    }
+
+    @Override
+    public void mapInitialized() {
+        MapOptions mapOptions = new MapOptions();
+
+        //mapOptions.center(x)
+        mapOptions.center(new LatLong(33.8869, 9.5375))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(9);
+
+        map = mapView.createMap(mapOptions);
+
     }
 
 }
