e38de938206a3c9aee6b074bd45bbcec0a7f7926
Mehdi Gaaloul <mehdi.gaaloul@esprit.tn>
2018-02-25 02:38:42 +0100

    map markers with click

--- Paw/src/paw/veterinaires/user/FXMLRechercheVetsController.java
+++ Paw/src/paw/veterinaires/user/FXMLRechercheVetsController.java
@@ -9,6 +9,7 @@ import Entity.Vets;
 import Service.VeterinaireServices;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.GoogleMap;
 import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.javascript.object.MapOptions;
@@ -19,6 +20,7 @@ import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
 import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
 import com.lynden.gmapsfx.service.geocoding.GeocodingService;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -28,7 +30,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import netscape.javascript.JSObject;
+import org.controlsfx.control.Rating;
 
 /**
  * FXML Controller class
@@ -47,8 +54,26 @@ public class FXMLRechercheVetsController implements Initializable, MapComponentI
     private GoogleMap map;
 
     private StringProperty address = new SimpleStringProperty();
-    
+
     ArrayList<Vets> list;
+    @FXML
+    private ImageView avatar;
+    @FXML
+    private Label nom;
+    @FXML
+    private Label tel;
+    @FXML
+    private Label adresse;
+    @FXML
+    private Label mail;
+    @FXML
+    private Label region;
+    @FXML
+    private Rating rate;
+    @FXML
+    private Label note;
+    @FXML
+    private StackPane afficheVet;
 
     /**
      * Initializes the controller class.
@@ -86,7 +111,7 @@ public class FXMLRechercheVetsController implements Initializable, MapComponentI
     public void mapInitialized() {
         geocodingService = new GeocodingService();
         MapOptions mapOptions = new MapOptions();
-        
+
         mapOptions.center(new LatLong(34.17372841, 9.76475011))
                 .mapType(MapTypeIdEnum.ROADMAP)
                 .overviewMapControl(false)
@@ -102,27 +127,47 @@ public class FXMLRechercheVetsController implements Initializable, MapComponentI
         VeterinaireServices service = new VeterinaireServices();
         //List<Vets> list = new ArrayList<Vets>();
         list = service.getList();
-        
-        
+
         //System.out.println(service.getList());
-        if(list.isEmpty()){
-            System.out.println("mouch mawjouda");
-        }else{System.out.println("mawjouda");}
-        
-        
+        if (list.isEmpty()) {
+            System.out.println("Liste vide");
+        } else {
+            System.out.println("Liste remplise");
+        }
+
         list.forEach((v) -> {
-           
+
             MarkerOptions markerOptions = new MarkerOptions();
-                markerOptions.position(new LatLong(v.getLatitude(), v.getLongitude()))
-                        .visible(Boolean.TRUE)
-                        .title("My Marker");
+            markerOptions.position(new LatLong(v.getLatitude(), v.getLongitude()))
+                    .visible(Boolean.TRUE)
+                    .title("My Marker");
 
-                Marker marker = new Marker(markerOptions);
-
-                map.addMarker(marker);
+            Marker marker = new Marker(markerOptions);
             
+            map.addMarker(marker);
+            map.addUIEventHandler(marker, UIEventType.click, (JSObject obj) -> {
+                afficheVet.setVisible(true);
+                DecimalFormat df = new DecimalFormat("#.#");
+                nom.setText(v.getNom() + " " + v.getPrenom());
+                mail.setText(v.getEmail());
+                tel.setText(String.valueOf(v.getNumero()));
+                adresse.setText(v.getAdresse());
+                region.setText(v.getRegion());
+                rate.setRating(v.getRate());
+                if ((v.getRate() != 0) && (v.getRate() != 1) && (v.getRate() != 2) && (v.getRate() != 3) && (v.getRate() != 4) && (v.getRate() != 5)) {
+                note.setText(String.valueOf(df.format(v.getRate())) + "/5");
+            } else {
+                note.setText(String.valueOf((v.getRate()).intValue()) + "/5");
+            }
+            });
+
         });
 
+    }
+
+    @FXML
+    private void fermer(ActionEvent event) {
+        afficheVet.setVisible(false);
     }
 
 }
